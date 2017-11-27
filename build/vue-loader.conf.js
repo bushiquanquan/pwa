40df2bbf72fde5768ff4130b6dc23519191c3666
yuanhuihui <yuanhuihui@meituan.com>
2017-11-27 14:45:46 +0800

    Pwa init

---
+++ build/vue-loader.conf.js
@@ -0,0 +1,21 @@
+/**
+ * @file vue-loader 配置文件
+ * @author yuan(908654876@qq.com)
+ */
+
+'use strict';
+
+const utils = require('./utils');
+const config = require('../config');
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    loaders: utils.cssLoaders({
+        sourceMap: isProduction
+            ? config.build.productionSourceMap
+            : config.dev.cssSourceMap,
+
+        // extract: isProduction
+        extract: true
+    })
+};
